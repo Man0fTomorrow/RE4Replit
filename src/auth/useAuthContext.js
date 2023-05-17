033fd604670c30823cea81c94b06e3e706ee9263
Man0fTomorrow <92611135+Man0fTomorrow@users.noreply.github.com>
2023-05-17 11:18:14 -0400

    FIREBASE CONFIG SETUP
    F

--- src/auth/useAuthContext.js
+++ src/auth/useAuthContext.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
-//
-import { AuthContext } from './JwtContext';
+// import { AuthContext } from './JwtContext';
+import { AuthContext } from './FirebaseContext';
+
 
 // ----------------------------------------------------------------------
 
